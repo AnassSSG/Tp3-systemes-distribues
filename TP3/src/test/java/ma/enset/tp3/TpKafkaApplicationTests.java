cd68dbf4f10396d0f5dd3c1177b085fd819fb74c
Anass boujendar <133109998+AnassSSG@users.noreply.github.com>
2024-10-28 00:04:51 +0100

    Add files via upload

---
+++ TP3/src/test/java/ma/enset/tp3/TpKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.enset.tp3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpKafkaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
